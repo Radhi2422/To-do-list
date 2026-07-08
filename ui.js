d6c473b357fc4665e537e63c44cc1c0585a1a1be
Radhika Mittal <106824237+Radhi2422@users.noreply.github.com>
2026-07-08 19:32:41 +0530

    Add files via upload

---
+++ ui.js
@@ -0,0 +1,60 @@
+/**
+ * Responsible for updating the interface.
+ * Function like:
+ * 
+ * renderTasks()
+ * enderTask()
+ * updateCounter()
+ * showEmptyState()
+ * showToast()
+ * openEditModal()
+ * 
+ * Only this file should create or update HTML elements.
+ */
+
+export function renderTasks(tasks){
+
+    const list=document.getElementById("taskList");
+
+    list.innerHTML="";
+
+    tasks.forEach(task=>{
+
+        const li=document.createElement("li");
+
+        li.innerHTML=`
+
+            <span>
+
+            ${task.completed ? "✔️" : ""}
+
+            ${task.title}
+
+            </span>
+            <button class="edit"
+                data-id="${task.id}">
+                Edit
+            </button>
+            <button class="complete"
+
+            data-id="${task.id}">
+
+            Complete
+
+            </button>
+
+            <button class="delete"
+
+            data-id="${task.id}">
+
+            Delete
+
+            </button>
+
+        `;
+
+        list.appendChild(li);
+
+    });
+
+}
